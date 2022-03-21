d12595d91c56494d52a34f92d3a7be4e2b4445b4
Mitchell Levy <levymitchell0@gmail.com>
2022-03-20 21:41:57 -0700

    Relicense code

---
+++ LICENSE.cc
@@ -0,0 +1,2 @@
+This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 Generic License.
+The full text of the license is available at https://creativecommons.org/licenses/by-sa/4.0/legalcode
